fa62d7e742e8a1d34f149c9bf8834510a56e634f
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 22:04:31 -0500

    took 'api' out of router path names

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,13 +13,13 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/burgers", function(req, res) {
   burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
   console.log("condition", condition);
   burger.updateOne(
