b339102217b6d132ba59b5580d91610f29d4f3d0
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 19:39:38 -0500

    added seed file

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Large Mac", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whooper", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Quarter Kilo", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Filet of Shrimp", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("4 Guys");
