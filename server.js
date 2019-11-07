c0e13c72d39f2c202eeffb48af09d6193deda70d
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 21:54:19 -0500

    added public folder with files, as well as editing server.js to see if anything happens

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 var express = require("express");
-var app = express();
-var exphbs = require("express-handlebars");
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+var app = express();
+
+app.use(express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.use(express.json());
 
+var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
-
-app.use(routes);
+app.use("/", routes);
 
 app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
