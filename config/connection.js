554389ab92aded8a10b62fd8b1fa5f40367cdda8
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 20:30:07 -0500

    finished orm with all 3 required methods, and fixed some typos in connection

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,9 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+} else {
     connection = mysql.createConnection({
         host: "localhost",
         port: 3306,    
@@ -13,14 +12,14 @@ else {
         password: "mArch1993!",
         database: "burgers_db"
     });
-}
+};
 
 connection.connect(function(err) {
     if (err) {
-        console.error("error connecting: " + err.stack);
+        console.error("Error Connecting: " + err.stack);
         return;
     }
-    console.log("connected as id " + connection.threadId);
+    console.log("Connected as ID " + connection.threadId);
 });
 
 module.exports = connection;
