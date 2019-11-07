ad58bd5c688e587f41fb3aca5c4c55ceaf487dd9
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 21:28:14 -0500

    forgot to set up serverjs - now donw

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,    
+        host: "localhost",    
         user: "root",
         password: "mArch1993!",
         database: "burgers_db"
--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 var express = require("express");
+var app = express();
+var exphbs = require("express-handlebars");
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
