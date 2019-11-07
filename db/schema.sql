2397ce68493775a309a25371a91c35f10a33dc57
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 19:49:24 -0500

    fixed type in seeds

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
     id INTEGER(11) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN NOT NULL
+    devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 )
