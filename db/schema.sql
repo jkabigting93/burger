73aced23f57d34c6ad1e7bcdbdb28e78b74f3282
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-06 21:04:02 -0500

    redid schema as per heroku deployment instructions

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN NOT NULL,
-    PRIMARY KEY (id)
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
