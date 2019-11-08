e1c1f6f40783871232e20b3a373bf4c95c073519
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-11-07 20:44:18 -0500

    simplified model js

--- models/burger.js
+++ models/burger.js
@@ -8,17 +8,13 @@ var burger = {
       });
     },
 
-    insertOne: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
-        cb(res);
-      });
-    },
+    insertOne: function(name, cb) {
+      orm.insertOne("burgers", name, cb);
+      },
 
-    updateOne: function(objColVals, condition, cb) {
-      orm.updateOne("burgers", objColVals, condition, function(res) {
-        cb(res);
-      });
+    updateOne: function(id, cb) {
+      orm.updateOne("burgers", id, cb);
     }
-  };
+  }
   
   module.exports = burger;
